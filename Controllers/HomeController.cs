abce70b34af68d12f5bd0dc3cb068ade188ce1dc
DESKTOP-C4R2BV5\User <muhamadhafiykharu@gmail.com>
2025-12-15 21:35:25 +0800

    Refactor HomeController to use IPlanRepository for plan retrieval and remove direct DbContext dependency

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SportMania.Data;
 using SportMania.Models;
+using SportMania.Repository.Interface;
 
 namespace SportMania.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ApplicationDbContext _db;
-    public HomeController(ApplicationDbContext db) => _db = db;
-
+    private readonly IPlanRepository _planRepository;
+    public HomeController(IPlanRepository planRepository)
+    {
+        _planRepository = planRepository;
+    } 
     public async Task<IActionResult> Index()
     {
-        var plans = await _db.Plans
-            .AsNoTracking()
-            .Include(p => p.Details)
-            .OrderBy(p => p.Name)
-            .ToListAsync();
-
+        var plans = await _planRepository.GetAllAsync();
         return View(plans);
     }
 
@@ -26,9 +23,6 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    // Keep Plan page if you still need it
-    public IActionResult Plan() => RedirectToAction(nameof(Index));
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
