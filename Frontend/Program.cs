0290d26d21e5db57049b2cf0adf2023d277e7b3c
DESKTOP-C4R2BV5\User <muhamadhafiykharu@gmail.com>
2026-01-31 00:59:33 +0800

    refactor: Clean up Blazor project files by removing unused package references and comments

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -3,7 +3,6 @@ using BlazorApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -11,25 +10,22 @@ builder.Services.AddRazorComponents()
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://localhost:5001";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
-// Add API services
+// Add Blazor services
 builder.Services.AddScoped<IPlanService, PlanService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
